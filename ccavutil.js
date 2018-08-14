1cdffd0918ab4fe533e732cd1913b3387d022e9a
Pikazza <karanit91@gmail.com>
2018-08-14 12:10:12 +0530

    changing digest method

--- ccavutil.js
+++ ccavutil.js
@@ -2,8 +2,8 @@ var crypto = require('crypto');
 exports.encrypt = function (plainText, workingKey) {
 	var m = crypto.createHash('md5');
     	m.update(workingKey);
-   	var key = m.digest();
-	//	let key = Buffer.from(m.digest("binary"), "binary");//buffer from binary string.
+   //	var key = m.digest();
+		let key = Buffer.from(m.digest("binary"), "binary");//buffer from binary string.
       	var iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f';
 	var cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
 	var encoded = cipher.update(plainText,'utf8','hex');
