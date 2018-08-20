30f1f68b89a7868921ecc40f7f6c897862079459
Pikazza <karanit91@gmail.com>
2018-08-20 14:16:34 +0530

    updating response hadlers

--- ccavResponseHandler.js
+++ ccavResponseHandler.js
@@ -10,11 +10,27 @@ exports.postRes = function(request,response){
 	ccavPOST = '';
 
         request.on('data', function (data) {
+          console.log("changes are for ccavResponse 1 "+data);
 	    ccavEncResponse += data;
+      console.log("changes are for ccavResponse 2 "+ccavEncResponse);
 	    ccavPOST =  qs.parse(ccavEncResponse);
+      console.log("changes are for ccavResponse 3 "+ccavPOST);
 	    var encryption = ccavPOST.encResp;
+      console.log("changes are for ccavResponse 4 "+encryption);
 	    ccavResponse = ccav.decrypt(encryption,workingKey);
+      console.log("changes are for ccavResponse 5 "+ccavResponse);
         });
+
+        request.on('error', (err) => {
+          // This prints the error message and stack trace to `stderr`.
+          ccavEncResponse += data;
+          ccavPOST =  qs.parse(ccavEncResponse);
+          var encryption = ccavPOST.encResp;
+          ccavResponse = ccav.decrypt(encryption,workingKey);
+          console.log("error block changes are for ccavResponse "+ccavResponse);
+          console.error(err.stack);
+        });
+
 
 	request.on('end', function () {
 	    var pData = '';
